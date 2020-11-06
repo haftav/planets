bde0bc3bf3b86f37512aea17460447a75fd643de
Tav Hafner <23143092+haftav@users.noreply.github.com>
2020-11-05 23:30:56 -0500

    Initial commit. Making some planets

---
+++ components/Orbiter.tsx
@@ -0,0 +1,55 @@
+import styles from 'styles/Orbiter.module.scss';
+
+interface OrbiterProps {
+  size?: number | string;
+  bgColor?: string;
+  animationDuration?: string;
+}
+
+const Orbiter = ({size = '100%', bgColor = 'white', animationDuration = '100s'}: OrbiterProps) => {
+  return (
+    <div
+      className={styles.wrapper}
+      style={{
+        width: size,
+        height: size,
+      }}
+    >
+      <div className={styles.innerWrapper}>
+        <div className={styles.svgWrapper}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="100%"
+            height="100%"
+            viewBox="-2 -2 100% 100%"
+          >
+            <circle
+              cx="50%"
+              cy="50%"
+              r="49%"
+              fill="transparent"
+              stroke="#818181"
+              strokeWidth="1"
+              stroke-dasharray="4"
+            />
+          </svg>
+        </div>
+        <div
+          className={styles.orbitalBodyContainer}
+          style={{
+            animationDuration,
+          }}
+        >
+          <div
+            className={styles.orbitalBody}
+            style={{
+              backgroundColor: bgColor,
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Orbiter;
---
+++ components/Sun.tsx
@@ -0,0 +1,5 @@
+import styles from 'styles/Sun.module.scss';
+
+const Sun = () => <div className={styles.sun}></div>;
+
+export default Sun;
