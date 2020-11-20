aba6b3351aeda1e40c539fe72eeab624758b5307
Tav Hafner <23143092+haftav@users.noreply.github.com>
2020-11-19 22:03:35 -0500

    Handling image loading

--- pages/planets/[planet].tsx
+++ pages/planets/[planet].tsx
@@ -1,5 +1,7 @@
+import {useEffect, useRef, useState} from 'react';
 import {useRouter} from 'next/router';
 import Link from 'next/link';
+import Image from 'next/image';
 import {motion} from 'framer-motion';
 
 import Layout from 'components/Layout';
@@ -7,16 +9,19 @@ import useData from 'hooks/useData';
 import useRouteSafeQuery from 'hooks/useRouteSafeQuery';
 
 import styles from 'styles/Planet.module.scss';
-import {useEffect, useRef, useState} from 'react';
 
 function isString(planet: string | string[]): planet is string {
   return typeof planet === 'string';
 }
 
 const Planet = () => {
+  const [imageLoaded, setImageLoaded] = useState(false);
   const planet = useRouteSafeQuery('planet');
-
   const {planets} = useData();
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
 
   if (!planet) {
     return null;
@@ -34,6 +39,12 @@ const Planet = () => {
 
   const {description} = planetData;
 
+  let imageClass = styles.image;
+
+  if (imageLoaded) {
+    imageClass += ` ${styles.imageLoaded}`;
+  }
+
   return (
     <motion.div
       exit={{opacity: 0}}
@@ -46,6 +57,15 @@ const Planet = () => {
     >
       <Layout>
         <div className={styles.wrapper}>
+          <Image
+            onLoad={handleImageLoad}
+            src="/planet.png"
+            alt="Picture of the author"
+            width={200}
+            height={200}
+            quality="50%"
+            className={imageClass}
+          />
           <h1>{planet.toUpperCase()}</h1>
           <p>{description}</p>
         </div>
